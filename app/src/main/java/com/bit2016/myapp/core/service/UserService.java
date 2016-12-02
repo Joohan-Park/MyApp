d3c5f7142418ef311c314759f2ac37c8e4f66fb9
Joohan-Park <wngksqkr91@naver.com>
2016-12-02 14:55:31 +0900

    1

--- app/src/main/java/com/bit2016/myapp/core/service/UserService.java
+++ app/src/main/java/com/bit2016/myapp/core/service/UserService.java
@@ -1,7 +1,13 @@
 package com.bit2016.myapp.core.service;
 
+import com.bit2016.android.network.JSONResult;
 import com.bit2016.myapp.core.domain.User;
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +17,23 @@ import java.util.List;
 
 public class UserService {
     public List<User> fetchUserList(){
+        String url = "http://192.168.1.82:8088/myapp-api/api/user/list";
+        HttpRequest httpRequest = HttpRequest.get(url);
+        httpRequest.contentType(HttpRequest.CONTENT_TYPE_JSON);
+        httpRequest.accept(HttpRequest.CONTENT_TYPE_JSON);
+        httpRequest.connectTimeout(3000);
+        httpRequest.readTimeout(3000);
+
+        int responseCode = httpRequest.code();
+        if(responseCode != HttpURLConnection.HTTP_OK){
+            throw new RuntimeException("HTTP Response:" + responseCode);
+        }
+
+        JSONResultUserList jsonResultUserList = fromJSON(httpRequest, JSONResultUserList.class);
+
+        return jsonResultUserList.getData();
+    }
+    public List<User> fetchUserMockList(){
 
         List<User> list = new ArrayList<User>();
 
@@ -25,4 +48,30 @@ public class UserService {
 
         return list;
     }
+
+    private class JSONResultUserList extends JSONResult<List<User>>{
+
+    }
+    /*
+    * Json 문자열을 자바 객체로 변환
+    * @param request
+    * @param target
+    * @param <V>
+    * @return
+    * */
+    protected <V> V fromJSON( HttpRequest request, Class<V> target ) {
+        V v = null;
+
+        try {
+            Gson gson = new GsonBuilder().create();
+
+            Reader reader = request.bufferedReader();
+            v = gson.fromJson(reader, target);
+            reader.close();
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return v;
+    }
+
 }
